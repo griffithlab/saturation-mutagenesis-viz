d4efbd381cfbda064fc361288237a4c9fed1b562
Zach Skidmore <zlskidmore@gmail.com>
2019-09-23 15:39:50 -0500

    added giacomelli code

--- server.R
+++ server.R
@@ -31,9 +31,9 @@ kato <- kato[,c("Transcript t1 NM_000546.5", "Protein P1 TP53_alpha  NP_000537.3
 colnames(kato) <- c("cDNA_variant", "p_variant", "Variant_Classification", "WAF1_Act",
                              "MDM2_Act", "BAX_Act", "_14_3_3_s_Act", "AIP_Act",
                              "GADD45_Act", "NOXA_Act", "p53R2_Act")
-kato <- kato[kato$Variant_Classification == "Missense_Mutation",]
+
+kato <- kato[kato$Variant_Classification == "Missense_Mutation" | kato$Variant_Classification == "Nonsense_Mutation",]
 kato$publication <- "kato et al. 2013"
-kato$Variant_Classification <- "non synonymous"
 
 ################# giacomelli et al. 2018 dataset ###############################
 
@@ -62,20 +62,38 @@ giacomelli$p_variant <- paste0("p.", giacomelli$AA_wt, giacomelli$Position, giac
 # giacomelli$test <- giacomelli$`A549_p53WT_Nutlin-3_Z-score`
 # 
 # ggplot(giacomelli, aes(x=test, fill=Variant_Classification)) + geom_density(alpha=.5)
+#browser()
+#ggplot(tmp, aes(x=AA_variant, y=Position)) + geom_tile() + scale_fill_viridis(aes(fill=`A549_p53WT_Nutlin-3_Z-score`))
+#tmp <- giacomelli[,c("AA_wt", "AA_variant", "Position", "A549_p53WT_Nutlin-3_Z-score")]
 
 ####################### combine papers #########################################
 
 # format the data for the promoter density plot
 promoterDensityPlotData_kato <- suppressWarnings(melt(kato, id.vars=c("cDNA_variant", "p_variant", "Variant_Classification", "publication")))
+promoterDensityPlotData_kato[promoterDensityPlotData_kato$Variant_Classification == "Nonsense_Mutation","value"] <- NA
+promoterDensityPlotData_kato$Variant_Classification <- "non synonymous"
 promoterDensityPlotData_kato$value <- suppressWarnings(as.numeric(as.character(promoterDensityPlotData_kato$value)))
+promoterHeatmapPlotData_kato <- promoterDensityPlotData_kato
 promoterDensityPlotData_kato <- as.data.frame(na.omit(promoterDensityPlotData_kato))
 
 promoterDensityPlotData_giacomelli <- suppressWarnings(melt(giacomelli[,c("cDNA_variant", "p_variant", "Variant_Classification", "publication", "A549_p53WT_Nutlin-3_Z-score", "A549_p53NULL_Nutlin-3_Z-score", "A549_p53NULL_Etoposide_Z-score")], id.vars=c("cDNA_variant", "p_variant", "Variant_Classification", "publication")))
 promoterDensityPlotData_giacomelli$value <- suppressWarnings(as.numeric(as.character(promoterDensityPlotData_giacomelli$value)))
 promoterDensityPlotData_giacomelli <- as.data.frame(promoterDensityPlotData_giacomelli)
+promoterHeatmapPlotData_giacomelli <- promoterDensityPlotData_giacomelli
 
 # combine the data
 promoterDensityPlotData <- rbind(promoterDensityPlotData_giacomelli, promoterDensityPlotData_kato)
+promoterHeatmapPlotData <- rbind(promoterHeatmapPlotData_giacomelli, promoterHeatmapPlotData_kato)
+
+######################## format data for heatmaps ##############################
+
+# split out amino acid positions
+promoterHeatmapPlotData$wt_aa <- gsub("p\\.\\(*([A-Z]+)([0-9]+)([A-Z]+)\\)*", "\\1", promoterHeatmapPlotData$p_variant, perl=T)
+promoterHeatmapPlotData$position_aa <- gsub("p\\.\\(*([A-Z]+)([0-9]+)([A-Z]+)\\)*", "\\2", promoterHeatmapPlotData$p_variant, perl=T)
+promoterHeatmapPlotData$mt_aa <- gsub("p\\.\\(*([A-Z]+)([0-9]+)([A-Z]+)\\)*", "\\3", promoterHeatmapPlotData$p_variant, perl=T)
+
+# remove na's
+promoterHeatmapPlotData <- promoterHeatmapPlotData[-which(is.na(promoterHeatmapPlotData$value)),]
 
 ################################################################################
 ##################### Define the shiny server ##################################
@@ -120,5 +138,14 @@ shinyServer(function(input, output, session){
     
     return(plot)
    })
+  
+  # heatmap plot
+  output$promoterHeatmapPlot <- renderPlot({
+    promoterHeatmapPlotData$position_aa <- factor(promoterHeatmapPlotData$position_aa, levels=as.character(1:394))
+    promoterHeatmapPlotData <- promoterHeatmapPlotData[promoterHeatmapPlotData$variable %in% c(input$promoterCheckGroup2_kato, input$promoterCheckGroup2_giacomelli),]
+    plot <- ggplot(promoterHeatmapPlotData, aes(x=mt_aa, y=position_aa, fill=value)) + geom_tile() + scale_fill_viridis() +
+      facet_wrap(~variable, drop=TRUE) + theme(axis.text.y=element_blank(), axis.ticks=element_blank())
+    return(plot)
+  })
 
 })
--- ui.R
+++ ui.R
@@ -97,6 +97,61 @@ navbarPage("CDI TP53 Project", theme="bootstrap.css",
                       )
                     )
                     
+                    ),
+           
+           # second tab for heatmap displays
+           tabPanel("Heatmap",
+                    
+                    fluidPage(
+                      
+                      fluidRow(
+                        
+                        column(12,
+                               
+                               wellPanel(
+                                 
+                                 fluidRow(
+                                   
+                                   column(6,
+                                          
+                                          # The checkbox widget for the promoter domains
+                                          checkboxGroupInput("promoterCheckGroup2_kato", label = h3("Kato et al. 2003"),
+                                                             choices = list("WAF1_Act" = "WAF1_Act",
+                                                                            "MDM2_Act"="MDM2_Act",
+                                                                            "BAX_Act" = "BAX_Act",
+                                                                            "_14_3_3_s_Act"="_14_3_3_s_Act",
+                                                                            "AIP_Act"="AIP_Act",
+                                                                            "GADD45_Act"="GADD45_Act",
+                                                                            "NOXA_Act"="NOXA_Act",
+                                                                            "p53R2_Act"="p53R2_Act"),
+                                                             selected = "WAF1_Act")),
+                                   
+                                   column(6,
+                                          
+                                          
+                                          checkboxGroupInput("promoterCheckGroup2_giacomelli", label = h3("giacomelli et al. 2018"),
+                                                             choices = list("p53NULL_Nutlin-3_Z-score" = "A549_p53NULL_Nutlin-3_Z-score",
+                                                                            "p53NULL_Etoposide_Z-score"="A549_p53NULL_Etoposide_Z-score",
+                                                                            "p53WT_Nutlin-3_Z-score" = "A549_p53WT_Nutlin-3_Z-score")))
+                                   
+                                 ),
+                                 
+                                 # second row to hold the density plot for kato et al.
+                                 fluidRow(
+                                   
+                                   # first column second row
+                                   column(12,
+                                          
+                                          # density plot for promoters
+                                          plotOutput('promoterHeatmapPlot')
+                                   )
+                                 )
+                                 
+                               )
+                               )
+                        
+                      )
+                    )
                     )
            )
 
